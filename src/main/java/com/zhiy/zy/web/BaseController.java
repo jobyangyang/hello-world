926bfa2aaa1ebab2f6272f533bc43c876dd39d3e
tonyone <yzyyinzhiyang@163.com>
2019-11-01 00:10:33 +0800

    测试前后交互完成。

--- src/main/java/com/zhiy/zy/web/BaseController.java
+++ src/main/java/com/zhiy/zy/web/BaseController.java
@@ -1,14 +1,29 @@
 package com.zhiy.zy.web;
 
+import com.zhiy.zy.domain.Employee;
+import com.zhiy.zy.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+import java.util.List;
+
 @RestController
 @EnableAutoConfiguration
 public class BaseController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
     @GetMapping("/")
     public String hello(){
         return "祖国你好";
+    }
+
+    @GetMapping("/h")
+    public List<Employee> getALl(){
+        return employeeService.getAll();
     }
 }
